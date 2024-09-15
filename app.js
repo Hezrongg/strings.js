48abd22c9fc9775d45195e181235a9b2af23a70e
Hezrongg <hezrongg26@gmail.com>
2024-09-15 11:51:17 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//CONCATENATION
+let favActorFirstName = "Robert";
+let favActorLastName = "Downey Jr.";
+let fullName = favActorFirstName + ' ' + favActorLastName;
+console.log(fullName);
+
+//LOWERCASE
+let upperCase = "ROBERT DOWNEY JR."
+console.log(upperCase.toLowerCase());
+
+//UPPERCASE
+let lowerCase = "garfield";
+console.log('My favorite pet name is ' + lowerCase.toUpperCase());
+
+//APPEND
+let message = "My favorite actor is " + fullName.toUpperCase() + ' ';
+message += "and his best show is Iron Man.";
+console.log(message);
